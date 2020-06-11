38f2835a82749d87e5fb7a79f02f24b41b905db7
steven <ssgeejr@mail.com>
2020-06-11 16:38:00 -0500

    test ready

--- src/main/java/com/tmobile/digital/aeolus/Aeolus.java
+++ src/main/java/com/tmobile/digital/aeolus/Aeolus.java
@@ -1,13 +1,22 @@
 package com.tmobile.digital.aeolus;
 
+import redis.clients.jedis.Jedis;
+
 public class Aeolus {
 
 	public Aeolus() {
-		// TODO Auto-generated constructor stub
+		try {
+			Jedis jedis = new Jedis("red2.f3gq02.clustercfg.use1.cache.amazonaws.com"); 
+			System.out.println("Connection to server sucessfully"); 
+			System.out.println("Server is running: " + jedis.ping());
+			//red2.f3gq02.clustercfg.use1.cache.amazonaws.com:6379
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Aeolus();
 
 	}
 
