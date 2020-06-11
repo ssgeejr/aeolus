f0c572e87a656e75549c57da8073c4cb2d1a0ae1
steven <ssgeejr@mail.com>
2020-06-11 16:11:45 -0500

    initial push

---
+++ src/main/java/com/tmobile/digital/aeolus/Aeolus.java
@@ -0,0 +1,14 @@
+package com.tmobile.digital.aeolus;
+
+public class Aeolus {
+
+	public Aeolus() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
